94cc386e412f17d10818742b38b3971f5a4f8620
Cooller-py <42335293+Cooller-py@users.noreply.github.com>
2018-08-17 10:22:43 +1000

    Update mail_sender.py

--- mail_sender.py
+++ mail_sender.py
@@ -3,17 +3,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import sys
 
-
-def progressBar(count, total, status=''):  # graphical display of the running script
-    bar_len = 6
-    filled_len = int(round(bar_len * count / float(total)))
-
-    percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-    sys.stdout.flush()  
-
 ###################################CONFIG################################
 HOST = "smtp."                          # host email
 SUBJECT = ""                            # message subject
@@ -25,20 +14,31 @@ fileText = 'msg.txt'                    # the name of the text file from which t
 fileMail = 'e_mail.txt'                 # the name of the text file from which the email list is taken
 ###########################################################################
 
+def progressBar(count, total, status=''):  # graphical display of the running script
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+
 def openFile(nameFile):
     f = open(nameFile, 'r')
     contentFile = f.read()
     f.close()
     return contentFile
 
-def sendMail():    
+def sendMail():
     s_email = openFile(fileMail).split() # convective string to slice
     server = smtplib.SMTP_SSL(HOST, 465)
     server.login(userName, passWord)
 
     total = len(s_email)
     count = 0
-  
+
     for i in s_email:
         msg = MIMEMultipart()
         msg['From'] = FROM
@@ -46,7 +46,7 @@ def sendMail():
         msg['To'] = str(i)
         msg.attach(MIMEText(openFile(fileText), 'plain'))
         server.send_message(msg)
-        
+
         count += 1
         progressBar(count, total, str(count))
 
